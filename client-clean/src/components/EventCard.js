f347632c6d98c7a77324ef14239c1467bcce7707
Frederik Kutsch <frederik.kutsch@tum.de>
2020-07-08 00:43:36 +0200

    Save Events via a button on the EventCard  and show upcoming saved  events on your Homepage and on the MyEvents page

--- client-clean/src/components/EventCard.js
+++ client-clean/src/components/EventCard.js
@@ -1,26 +1,88 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 class EventCard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { organizer: {} };
+    this.state = {
+      organizer: {},
+      interestedInEvents: [],
+      interestedIn: false,
+      redirect: null,
+    };
   }
+
+  days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  fetchInterestedInEvents = () => {
+    var component_scope = this;
+    return new Promise((resolve, reject) => {
+      fetch("/profile", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          Authorization: "Bearer " + window.sessionStorage.secret_token,
+        },
+      })
+        .then((res) => res.json(res))
+        .then(function (res) {
+          component_scope.setState({
+            interestedInEvents: res.interestedInEvents,
+          });
+          resolve(res);
+        })
+        .catch((err) => reject(err));
+    });
+  };
 
   componentDidMount() {
+    var component_scope = this;
     // Get the organizer corresponding to the event given in the props
-    fetch(`/users/${this.props.event.organizer}`)
+    fetch(`/users/${component_scope.props.event.organizer}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        this.setState({ organizer: data });
+        component_scope.setState({ organizer: data });
       })
       .catch(console.log);
+
+    // Get the events the currently logged in user is interested in and check if the event this EventCard belongs to is in there
+    if (window.sessionStorage.secret_token != null) {
+      this.fetchInterestedInEvents().then(() => {
+        if (this.state.interestedInEvents.includes(this.props.event._id)) {
+          this.setState({ interestedIn: true });
+        }
+      });
+    }
   }
 
+  // Currently not in use, not sure if we want that shown here
   getType = () => {
-    let type = this.props.event.type;
+    var component_scope = this;
+    let type = component_scope.props.event.type;
     switch (type) {
       case "course":
         type = "dance course";
@@ -32,43 +94,131 @@ class EventCard extends React.Component {
         type = "dance party";
         break;
       default:
-        type = type;
+        alert("Unknown event type passed to EventCard");
     }
     return type;
   };
 
-  getDate = () => {
-    // TODO: make proper date object from string and use date methods to extract weekday
-    let date = this.props.event.startDate.substring(0, 10);
-    return date;
+  handleSave = () => {
+    var component_scope = this;
+    if (window.sessionStorage.secret_token != null) {
+      component_scope.setState({ interestedIn: true });
+      // Refresh the state from the backend
+      component_scope.fetchInterestedInEvents().then(() => {
+        // Add the event to interestedInEvents in the state
+        component_scope.setState(
+          {
+            interestedInEvents: [
+              ...component_scope.state.interestedInEvents,
+              component_scope.props.event._id,
+            ],
+          },
+          // Callback of setState so it definitely pushes the newest state
+          () => {
+            // Push that new state to the backend
+            fetch("/profile/update", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                Authorization: "Bearer " + window.sessionStorage.secret_token,
+              },
+              body: JSON.stringify([
+                {
+                  propName: "interestedInEvents",
+                  value: component_scope.state.interestedInEvents,
+                },
+              ]),
+            })
+              .then((res) => res.json(res))
+              .catch((err) => console.log(err));
+          }
+        );
+      });
+    } else {
+      this.setState({ redirect: "/login" });
+    }
+  };
+
+  handleUnSave = () => {
+    var component_scope = this;
+    component_scope.setState({ interestedIn: false });
+    // Refresh the state from the backend
+    component_scope.fetchInterestedInEvents().then(() => {
+      // Remove the event from interestedInEvents in the state
+      component_scope.setState(
+        {
+          interestedInEvents: component_scope.state.interestedInEvents.filter(
+            (item) => item !== component_scope.props.event._id
+          ),
+        },
+        () => {
+          // Push that new state to the backend
+          fetch("/profile/update", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json; charset=utf-8",
+              Authorization: "Bearer " + window.sessionStorage.secret_token,
+            },
+            body: JSON.stringify([
+              {
+                propName: "interestedInEvents",
+                value: component_scope.state.interestedInEvents,
+              },
+            ]),
+          })
+            .then((res) => res.json(res))
+            .catch((err) => alert(err));
+        }
+      );
+    });
   };
 
   render() {
+    const date = new Date(this.props.event.startDate);
+    if (this.state.redirect) {
+      return <Redirect push to={this.state.redirect} />;
+    }
     return (
-      <Link
-        to={`/events/${this.props.event._id}`}
-        style={{ textDecoration: "none", color: "black" }}
+      <Card
+        id={this.props.event._id}
+        style={{ width: "18rem" }}
+        className="m-2"
       >
-        <Card
-          id={this.props.event._id}
-          style={{ width: "18rem" }}
-          className="m-2"
-        >
+        <Link to={`/events/${this.props.event._id}`}>
           <Card.Img
             variant="top"
-            src={this.props.event.picture}
+            src={
+              this.props.event.picture
+                ? this.props.event.picture
+                : "img/placeholder2_1024x365.png"
+            }
             alt={this.props.event.title}
             style={{ objectFit: "cover", width: "286px", height: "180px" }}
           />
-          <Card.Body>
+        </Link>
+        <Card.Body>
+          <Link
+            to={`/events/${this.props.event._id}`}
+            style={{ textDecoration: "none", color: "black" }}
+          >
             <Card.Title>{this.props.event.title}</Card.Title>
-            <Card.Text>
-              {this.getType()} {this.getDate()}
-            </Card.Text>
-            <Card.Text>{this.state.organizer.name}</Card.Text>
-          </Card.Body>
-        </Card>
-      </Link>
+          </Link>
+          <Card.Subtitle className="mb-2 text-muted">
+            {this.days[date.getDay()]}, {date.getDate()}{" "}
+            {this.months[date.getMonth()]} {date.getFullYear()}
+          </Card.Subtitle>
+          <Card.Text>by {this.state.organizer.name}</Card.Text>
+          {this.state.interestedIn ? (
+            <Button variant="success" onClick={() => this.handleUnSave()}>
+              Saved
+            </Button>
+          ) : (
+            <Button variant="secondary" onClick={() => this.handleSave()}>
+              Save in <i>My Events</i>
+            </Button>
+          )}
+        </Card.Body>
+      </Card>
     );
   }
 }
